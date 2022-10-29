087a3985738789ce6339c92b24699f435c3e2da1
yanzhicong <1069163331@qq.com>
2022-10-29 14:19:47 +0000

    update

--- collect_result.py
+++ collect_result.py
@@ -1,7 +1,13 @@
 import os
 import sys
+import argparse
+import re
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--include_exp", default=None, type=str)
+parser.add_argument("-e", "--exclude_exp", default=None, type=str)
+args = parser.parse_args()
 
 
 experiment_dir = "sku110_logdir"
@@ -16,15 +22,35 @@ def find_and_print_result(file_path):
         for line in infile:
             if line.startswith(" Average Precision  (AP) @[ IoU") and line.find("area=   all") != -1:
                 key, value = line.strip().split(" = ")
-                result_dict[key.strip()] = float(value.strip())
+                key, value = key.strip(), float(value.strip())
+                # result_dict[key] = result_dict.get(key, []).append(value)
+                if key in result_dict:
+                    result_dict[key].append(value)
+                else:
+                    result_dict[key] = [value,]
+                # result_dict[key.strip()] = float(value.strip())
 
     for key, value in result_dict.items():
-        print("\t{} = {}".format(key, value))            
+        print("\t{} = {}".format(key, value))
 
     print()
 
 if __name__ == "__main__":
+
+
     for exp_name in os.listdir(experiment_dir):
-        find_and_print_result(os.path.join(experiment_dir, exp_name, "workerlog.0"))
-        find_and_print_result(os.path.join(experiment_dir, exp_name, "eval_result.txt"))
-        find_and_print_result(os.path.join(experiment_dir, exp_name, "test_result.txt"))
+
+        for file_name in [
+            "workerlog.0",
+            "eval_result.txt",
+            "test_result.txt",
+        ]:
+
+            result_path = os.path.join(experiment_dir, exp_name, file_name)
+            if args.include_exp is not None and not re.match(args.include_exp, result_path):
+                continue
+            if args.exclude_exp is not None and re.match(args.exclude_exp, result_path):
+                continue
+
+            find_and_print_result(result_path)
+        
